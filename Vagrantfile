04b2f5adef1bcd86c7606a188baf6e9fb958ef65
Aleksandr Guljajev <aleksandr.guljajev@gmbill.com>
2014-10-31 11:34:06 +0100

    Fixed docker provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,5 @@
 Vagrant.configure("2") do |config|
-  #config.vm.box = "ubuntu/trusty64"
-  config.vm.box = "package.box"
+  config.vm.box = "ubuntu/trusty64"
   config.vm.synced_folder ".", "/vagrant/", :nfs => true
   config.vm.network :private_network, ip: "10.0.1.2"
 
--- puppet/modules/docker/manifests/init.pp
+++ puppet/modules/docker/manifests/init.pp
@@ -1,13 +1,29 @@
 class docker {
-  exec { "add_docker":
+  exec { "add-docker-key":
     command => "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9",
     path => "/bin:/usr/bin:/usr/sbin",
+    before => Exec["apt-get update"],
+    require => [Exec["apt-key net-update"], Package["apt-transport-https"]]
   }
+  exec {"apt-key net-update":
+    command => "apt-key net-update",
+    path => "/bin:/usr/bin/:/sbin",
+    returns => [0, 1] #ignore return code, it can be 1
+  }    
   file { "/etc/apt/sources.list.d/docker.list":
     source => "puppet:///modules/docker/docker.list",
-    before => Package["lxc-docker"],
+    before => Exec["add-docker-key"],
+    owner => root
   }
   package {"lxc-docker":
     ensure => "installed",
+    require => Exec['apt-get update'],
+  }
+  package {"apt-transport-https":
+    ensure => "installed"
+  }
+  exec {"apt-get update":
+    command => "apt-get -y update",
+    path => "/bin:/usr/bin:/sbin"
   }
 }
