565820326d5a201d61f800f7b379daf236c0ef58
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2014-11-03 21:48:50 +0100

    Updating and nginx modules

--- puppet/modules/dns/manifests/init.pp
+++ puppet/modules/dns/manifests/init.pp
@@ -1,25 +1,29 @@
 class dns {
   package {"bind9":
     ensure => "installed"
-  }
-  file {"/etc/bind/named.conf":
-    source => "puppet:///modules/dns/named.conf",
-    require => Package["bind9"]
-  }
-  file {"/etc/bind/named.conf.local":
-    source => "puppet:///modules/dns/named.conf.local",
-    require => Package["bind9"]
-  }
-  file {"/etc/bind/zones/planetarium.db":
-    source => "puppet:///modules/dns/planetarium.db",
-    require => Package["bind9"]
-  }
-  file {"/etc/bind/zones/rev.1.0.10.in-addr.arpa":
-    source => "puppet:///modules/dns/rev.1.0.10.in-addr.arpa",
-    require => Package["bind9"]
-  }
-  file {"/etc/bind/named.conf.options":
-    source => "puppet:///modules/dns/named.conf.options",
-    require => Package["bind9"]
-  }  
+    } ->
+    file {"/etc/bind/named.conf":
+      source => "puppet:///modules/dns/named.conf",
+      require => Package["bind9"]
+      } ~>
+      file {"/etc/bind/named.conf.local":
+        source => "puppet:///modules/dns/named.conf.local",
+        require => Package["bind9"]
+        } ~> 
+        file {"/etc/bind/db.planetarium":
+          source => "puppet:///modules/dns/db.planetarium",
+          require => Package["bind9"]
+          } ~>
+          file {"/etc/bind/rev.1.0.10.in-addr.arpa":
+            source => "puppet:///modules/dns/rev.1.0.10.in-addr.arpa",
+            require => Package["bind9"]
+            } ~>
+            file {"/etc/bind/named.conf.options":
+              source => "puppet:///modules/dns/named.conf.options",
+              require => Package["bind9"]
+              } ~>
+              service {"bind9":
+                ensure    => "running",
+                enable    => true,
+              }
 }
--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,14 +1,18 @@
 class nginx {
   package {"nginx":
     ensure => "installed"
-  }
-  file {"/home/vagrant/vagrant-hosts":
-    source => "puppet:///modules/nginx/vagrant-hosts"
-  }
-  file {"/etc/nginx/sites-enabled/vagrant-hosts":
-    ensure => "link",
-    target => "/home/vagrant/vagrant-hosts",
-    owner => "vagrant",
-    require => Package["nginx"]
-  }
+    } ->
+    file {"/home/vagrant/vagrant-hosts":
+      source => "puppet:///modules/nginx/vagrant-hosts"
+      } ->
+      file {"/etc/nginx/sites-enabled/vagrant-hosts":
+        ensure => "link",
+        target => "/home/vagrant/vagrant-hosts",
+        owner => "vagrant",
+        require => Package["nginx"]
+        } ~>
+        service {"nginx":
+          ensure => "running",
+          enable => true
+        }
 }
